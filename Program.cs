0629c439e0975fe5d6de545395eddee78263ab4e
glukov <glukov@mail.bg>
2019-09-03 07:22:33 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace UpnetixExam
+{
+    [TestFixture]
+    public class Program
+    {
+
+        private IWebDriver driver;
+
+        private string usedURL = "https://www.abv.bg/";
+        private string userName = "g.lukov_test";
+        private string passWord = "Test1234";
+        private string myName = "GGGG LLLL";
+        private string searchMenuId = "searchFieldInbox";
+        private string searchStr = "thecrow";
+        private string subjectOfMail = "test mail";
+        private string mailBody = "mail recieved";
+
+        [SetUp]
+        public void Initialize()
+        {
+            ChromeOptions options = new ChromeOptions();
+
+            options.AddArguments("--incognito");
+
+            driver = new ChromeDriver(options);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+
+        [Test, Order(1)]
+        public void VerifySuccessfullLogIn()
+        {
+            SeleniumMethods.LogInToABV(driver, usedURL, userName, passWord);
+
+            /*Console.WriteLine(myName);
+            Console.WriteLine(SeleniumMethods.GetUserNameFromMainPage(driver));
+
+            Console.WriteLine(searchMenuId);
+            Console.WriteLine(SeleniumMethods.SearchMenu(driver));*/
+
+            Assert.AreEqual(myName, SeleniumMethods.GetUserNameFromMainPage(driver));
+            Assert.AreEqual(searchMenuId, SeleniumMethods.SearchMenu(driver));
+
+        }
+
+        [Test, Order(2)]
+        public void VerifyMailSubject()
+        {
+            SeleniumMethods.LogInToABV(driver, usedURL, userName, passWord);
+
+            SeleniumMethods.PerformSearch(driver, searchStr);
+
+            /*Console.WriteLine(subjectOfMail);
+            Console.WriteLine(SeleniumMethods.MailSubject(driver));*/
+
+            Assert.AreEqual(subjectOfMail, SeleniumMethods.GetMailSubject(driver));
+
+        }
+
+        [Test, Order(3)]
+        public void VerifyMailIsRecieved()
+        {
+            SeleniumMethods.LogInToABV(driver, usedURL, userName, passWord);
+
+            SeleniumMethods.PerformSearch(driver, searchStr);
+
+            SeleniumMethods.OpenUnreadMail(driver);
+
+            SeleniumMethods.ReplicationOfTheMail(driver, userName);
+
+            SeleniumMethods.AddTextToMail(driver, mailBody);
+
+            SeleniumMethods.SendMail(driver);
+
+        }
+
+    }
+}
---
+++ SeleniumMethods.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Interactions;
+
+namespace UpnetixExam
+{
+    class SeleniumMethods
+    {
+        public static void LogInToABV(IWebDriver driver, string usedURL, string userName, string passWord)
+        {
+            driver.Navigate().GoToUrl(usedURL);
+
+            SeleniumMethods.CloseGDPRiFrame(driver);
+
+            driver.SwitchTo().DefaultContent();
+
+            driver.FindElement(By.Id("username")).SendKeys(userName);
+
+            driver.FindElement(By.Id("password")).SendKeys(passWord);
+
+            driver.FindElement(By.Id("loginBut")).Click();
+
+        }
+        
+        public static string GetUserNameFromMainPage(IWebDriver driver)
+        {
+            IWebElement userName = driver.FindElement(By.CssSelector("#middlePagePanel .h1 .userName"));
+
+            return userName.Text;
+        }
+
+        public static string SearchMenu(IWebDriver driver)
+        {
+            IWebElement searchMenu = driver.FindElement(By.CssSelector("#searchFieldInbox"));
+
+            return searchMenu.GetAttribute("Id");
+        }
+
+        public static void PerformSearch(IWebDriver driver, string searchStr)
+        {
+            IWebElement searchMenu = driver.FindElement(By.CssSelector("#searchFieldInbox"));
+
+            searchMenu.SendKeys(searchStr);
+
+            driver.FindElement(By.CssSelector("#searchlens")).Click();
+
+        }
+
+        public static string GetMailSubject(IWebDriver driver)
+        {
+            IWebElement mailSubject = driver.FindElement(By.CssSelector("#inboxTable td:nth-child(5)"));
+
+            return mailSubject.Text;
+        }
+
+        public static void OpenUnreadMail(IWebDriver driver)
+        {
+            driver.FindElement(By.CssSelector("#inboxTable td:nth-child(5)")).Click();
+        }
+
+        public static void ReplicationOfTheMail(IWebDriver driver, string userName)
+        {
+            driver.FindElement(By.CssSelector(".abv-letterLinksHolder>div:nth-child(1)")).Click();
+
+            SeleniumMethods.AddCopyToTheReplication(driver, userName);
+        }
+
+        public static void AddCopyToTheReplication(IWebDriver driver, string userName)
+        {
+            driver.FindElement(By.CssSelector(".ccBccButtons>div:nth-child(1)")).Click();
+
+            driver.FindElement(By.CssSelector("tr:nth-child(3)>td.clientField>div>input")).SendKeys(userName + "@abv.bg");
+        }
+
+        public static void AddTextToMail(IWebDriver driver, string mailBody)
+        {
+            driver.FindElement(By.CssSelector("iframe.gwt-RichTextArea")).SendKeys(mailBody);
+        }
+
+        public static void SendMail(IWebDriver driver)
+        {
+            driver.FindElement(By.CssSelector("div.sendMenuContent>div:nth-child(1)")).Click();
+        }
+
+        private static void CloseGDPRiFrame(IWebDriver driver)
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));            
+
+            driver.SwitchTo().Frame("abv-GDPR-frame");
+
+            wait.Until((w) => w.FindElement(By.Id("cmp-faktor-io")));
+
+            int IsElementExist = driver.FindElements(By.Id("cmp-faktor-io")).Count;
+           
+            if (IsElementExist == 1)
+            {
+                driver.SwitchTo().Frame("cmp-faktor-io");
+
+                wait.Until((w) => w.FindElement(By.CssSelector(".theme-wrapper .actions")));
+
+                driver.FindElement(By.CssSelector(".theme-wrapper .actions")).Click();
+            }
+            else
+            {
+                return;
+            }
+        }
+
+    }
+}
